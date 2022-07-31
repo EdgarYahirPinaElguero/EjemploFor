73b00ec2475428726c97ef51bdcf5ef00d829c9b
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:22:36 -0500

    Create EjemploFor.cpp

---
+++ EjemploFor.cpp
@@ -0,0 +1,12 @@
+package CicloFor;
+
+public class EjemploFor {
+		public static void main (String[] args) {
+			int i;
+			
+			for (i=10; i>=1; i--) {
+				System.out.println(i+ " ");
+			}
+			System.out.println("\nFin Programa");
+		}
+}
